1af28ee16e4fbe978563aa3e879168b9d7806e7e
kabanovaen <kabanovaen@mail.ru>
2018-10-31 17:01:27 +0300

    New feature in master

--- Solution.java
+++ Solution.java
@@ -2,5 +2,6 @@ public class Solution {
     public static void main(String[] args) {
 	System.out.println("Hello, Git!");
 	System.out.println("My Commit");
+	System.out.println("New feature in master");
 	}
 }
