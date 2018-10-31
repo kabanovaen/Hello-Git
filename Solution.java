74094c206ffa36da8e323117e259d01236457467
kabanovaen <kabanovaen@mail.ru>
2018-10-31 18:02:47 +0300

    Fix

--- Solution.java
+++ Solution.java
@@ -2,10 +2,6 @@ public class Solution {
     public static void main(String[] args) {
 	System.out.println("Hello, Git!");
 	System.out.println("My Commit");
-<<<<<<< HEAD
-	System.out.println("New feature in master");
-=======
 	System.out.println("Here is new feature");
->>>>>>> feature_1
 	}
 }
